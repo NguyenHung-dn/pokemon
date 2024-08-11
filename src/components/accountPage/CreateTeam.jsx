91203db45ac76fe20a3c60083aee63bc5d9447d3
NguyenHung-dn <nguyenhungdn0109@gmail.com>
2024-08-11 23:37:15 +0700

    add modal popUp, up img pokemon

--- src/components/accountPage/CreateTeam.jsx
+++ src/components/accountPage/CreateTeam.jsx
@@ -1,42 +1,9 @@
 import axios from "axios";
-import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
-import PokemonSelectionModal from "./PokemonSelectionModal";
-import AbilityMoveSelectionModal from "./AbilityMoveSelectionModal";
-import PokemonInformationModal from "./PokemonInformationModal";
-
 export default function CreateTeam({
   userData,
   fetchDataUserTeams,
-  selectedTeamIndex,
-  teamPokemon,
   handleTeamClick,
-  setTeamPokemon,
-  setSelectedTeamIndex,
-  setShowPokemonModal, // Nhận prop từ AccountPage
-  setSelectedPokemon, // Nhận prop từ AccountPage
-  setShowAbilityMoveModal, // Nhận prop từ AccountPage
-  setShowPokemonInfoModal, // Nhận prop từ AccountPage
-  setPokemonInfo, // Nhận prop từ AccountPage
 }) {
-  const [pokemonListName, setPokemonListName] = useState([]);
-  const token = Cookies.get("token");
-
-  useEffect(() => {
-    const fetchPokemon = async () => {
-      try {
-        const response = await axios.get(
-          "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151"
-        );
-        setPokemonListName(response.data.results);
-      } catch (error) {
-        console.error("Failed to fetch Pokémon data:", error);
-      }
-    };
-
-    fetchPokemon();
-  }, []);
-  console.log("pokemonListName", pokemonListName);
   const handleCreateTeam = async () => {
     // const token = Cookies.get("token");
     if (!token) {
@@ -59,171 +26,29 @@ export default function CreateTeam({
       );
     }
   };
-
-  // const handleSaveTeam = async () => {
-  //   const token = Cookies.get("token");
-  //   if (!token) {
-  //     alert("Token không hợp lệ hoặc không có.");
-  //     return;
-  //   }
-
-  //   if (selectedTeamIndex === null) {
-  //     alert("No team selected.");
-  //     return;
-  //   }
-
-  //   try {
-  //     await axios.post("https://pokemon-api-phi-nine.vercel.app/team/update", {
-  //       token,
-  //       team: {
-  //         _id: userData.teams[selectedTeamIndex]._id,
-  //         team: teamPokemon,
-  //       },
-  //     });
-  //     fetchDataUserTeams(); // Cập nhật dữ liệu đội hình
-  //     alert("Team updated successfully");
-  //   } catch (error) {
-  //     console.error("Failed to update team:", error);
-  //     alert(
-  //       "Failed to update team: " +
-  //         (error.response?.data?.message || error.message)
-  //     );
-  //   }
-  // };
-  // const addPokemonToTeam = (pokemon) => {
-  //   setTeamPokemon((prevTeam) => {
-  //     if (prevTeam.length < 6) {
-  //       return [...prevTeam, pokemon];
-  //     } else {
-  //       alert("A team can only have up to 6 Pokémon.");
-  //       return prevTeam;
-  //     }
-  //   });
-  //   setShowPokemonModal(false);
-  // };
-
-  // const handleSelectPokemonModal = (pokemon) => {
-  //   setSelectedPokemon(pokemon);
-  //   setShowPokemonModal(true);
-  // };
-  // const handleSaveAbilityMove = (pokemon) => {
-  //   setTeamPokemon((prevTeam) =>
-  //     prevTeam.map((poke) => (poke.name === pokemon.name ? pokemon : poke))
-  //   );
-  //   setShowAbilityMoveModal(false);
-  // };
-
-  // const handlePokemonInfo = (pokemon) => {
-  //   const fetchPokemonDetails = async (pokemonName) => {
-  //     try {
-  //       const response = await axios.get(
-  //         `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-  //       );
-  //       const { name, id, types, abilities, moves } = response.data;
-  //       setPokemonInfo({
-  //         name,
-  //         id,
-  //         type: types.map((typeInfo) => typeInfo.type.name).join(", "),
-  //         abilities: abilities.map((abilityInfo) => abilityInfo.ability.name),
-  //         moves: moves.map((moveInfo) => moveInfo.move.name),
-  //       });
-  //       setShowPokemonInfoModal(true);
-  //     } catch (error) {
-  //       console.error("Failed to fetch Pokémon details:", error);
-  //     }
-  //   };
-
-  //   fetchPokemonDetails(pokemon.name);
-  // };
-
-  // const handleEditPokemon = (pokemon) => {
-  //   setSelectedPokemon(pokemon);
-  //   setShowAbilityMoveModal(true);
-  // };
-
-  // const handleSelectPokemon = (pokemon) => {
-  //   setTeamPokemon((prevTeam) =>
-  //     prevTeam.map((poke) => (poke.name === pokemon.name ? pokemon : poke))
-  //   );
-  //   setShowAbilityMoveModal(true);
-  // };
-
-  const imgId = pokemonListName.map((p) => {
-    const urlParts = p.url.split("/");
-    const id = urlParts[urlParts.length - 2];
-    return { name: p.name, id: Number(id) };
-  });
   return (
-    <section className="flex gap-4">
-      <div className="h-1000px w-375px bg-primary">
-        <h1 className="font-press_start_2p">MyTeam</h1>
-        <div className="flex flex-col">
-          {userData &&
-            userData.teams.map((u, index) => (
-              <button key={index} onClick={() => handleTeamClick(index)}>
-                Team {index + 1}
-              </button>
-            ))}
-          {userData && userData.teams.length < 5 && (
-            <button onClick={handleCreateTeam}>Add Team</button>
-          )}
-        </div>
+    <div className="h-1000px w-375px bg-primary flex flex-col  text-center">
+      <h1 className="font-press_start_2p mt-10 mb-10">MyTeam</h1>
+      <div className="flex flex-col gap-4 items-center rounded-xl">
+        {userData &&
+          userData.teams.map((u, index) => (
+            <button
+              className=" bg-white w-80 rounded-xl py-2"
+              key={index}
+              onClick={() => handleTeamClick(index)}
+            >
+              Team {index + 1}
+            </button>
+          ))}
+        {userData && userData.teams.length < 5 && (
+          <button
+            className=" w-80 py-2  bg-white border border-primary text-black"
+            onClick={handleCreateTeam}
+          >
+            Add Team
+          </button>
+        )}
       </div>
-      {/* <div>
-        {selectedTeamIndex !== null &&
-          userData &&
-          userData.teams[selectedTeamIndex] && (
-            <>
-              <div>
-                Choose Pokémon
-                <div className="flex gap-3">
-                  {teamPokemon.map((pokemon) => {
-                    return (
-                      <div
-                        key={pokemon.name}
-                        className="flex items-center gap-3 justify-between p-2 bg-gray-100 rounded mb-2"
-                      >
-                        <span>{pokemon.name}</span>
-                        <button
-                          className="text-blue-500  bg-yellow-400 "
-                          onClick={() => handlePokemonInfo(pokemon)}
-                        >
-                          Info
-                        </button>
-                        <button
-                          className="text-blue-500 bg-primary "
-                          onClick={() => handleEditPokemon(pokemon)}
-                        >
-                          Edit
-                        </button>
-                        <button
-                          className="text-fire p-2   bg-gray-700"
-                          onClick={() => {
-                            handleSelectPokemonModal(pokemon);
-                          }}
-                        >
-                          Select New Pokémon
-                        </button>
-                      </div>
-                    );
-                  })}
-                  {teamPokemon.length < 4 && (
-                    <button onClick={() => setShowPokemonModal(true)}>
-                      Add Pokémon
-                    </button>
-                  )}
-                </div>
-              </div>
-              <div className="h-80 w-785px bg-steel">Information</div>
-              <button
-                onClick={handleSaveTeam}
-                className="mt-4 p-2 bg-green-500 text-white rounded hover:bg-green-700"
-              >
-                Save Team
-              </button>
-            </>
-          )}
-      </div> */}
-    </section>
+    </div>
   );
 }
--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -7,18 +7,21 @@ import { useEffect, useState } from "react";
 import PokemonSelectionModal from "@/components/accountPage/PokemonSelectionModal"; // Di chuyển import này từ CreateTeam
 import AbilityMoveSelectionModal from "@/components/accountPage/AbilityMoveSelectionModal";
 import PokemonInformationModal from "@/components/accountPage/PokemonInformationModal";
+import { getAllPokemonDetails } from "@/services/pokemonService";
+import PokemonOptionPopup from "@/components/accountPage/PokemonOptionPopup";
+
 export default function AccountPage() {
   const [userData, setUserData] = useState("");
   const [error, setError] = useState(null);
   const [selectedTeamIndex, setSelectedTeamIndex] = useState(null);
   const [teamPokemon, setTeamPokemon] = useState([]);
   const cookie = Cookies.get("token");
-
-  const [showPokemonModal, setShowPokemonModal] = useState(false); // Di chuyển từ CreateTeam
-  const [selectedPokemon, setSelectedPokemon] = useState(null); // Di chuyển từ CreateTeam
-  const [showAbilityMoveModal, setShowAbilityMoveModal] = useState(false); // Di chuyển từ CreateTeam
-  const [showPokemonInfoModal, setShowPokemonInfoModal] = useState(false); // Di chuyển từ CreateTeam
-  const [pokemonInfo, setPokemonInfo] = useState(null); // Di chuyển từ CreateTeam
+  const [showPokemonModal, setShowPokemonModal] = useState(false);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [showAbilityMoveModal, setShowAbilityMoveModal] = useState(false);
+  const [showPokemonInfoModal, setShowPokemonInfoModal] = useState(false);
+  const [pokemonInfo, setPokemonInfo] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
 
   const fetchDataUserTeams = async () => {
     try {
@@ -34,9 +37,20 @@ export default function AccountPage() {
       console.log(error);
     }
   };
+
+  // Hàm gọi API và lưu dữ liệu Pokémon
+  const fetchPokemonData = async () => {
+    try {
+      const allPokemonDetails = await getAllPokemonDetails();
+      setPokemonInfo(allPokemonDetails);
+    } catch (error) {
+      console.error("Failed to fetch Pokémon details:", error);
+    }
+  };
   useEffect(() => {
     fetchDataUserTeams();
-  }, [cookie]);
+    fetchPokemonData();
+  }, []);
 
   const handleTeamClick = (index) => {
     setSelectedTeamIndex(index);
@@ -94,6 +108,12 @@ export default function AccountPage() {
     }
   };
 
+  const handleSelectNewPokemon = (pokemon) => {
+    setShowPopup(false);
+    // Hiển thị modal để chọn Pokémon mới
+    setShowPokemonModal(true);
+  };
+
   const handleEditPokemon = (pokemon) => {
     setSelectedPokemon(pokemon);
     setShowAbilityMoveModal(true);
@@ -102,30 +122,45 @@ export default function AccountPage() {
     setSelectedPokemon(pokemon);
     setShowPokemonModal(true);
   };
-  const handlePokemonInfo = (pokemon) => {
-    const fetchPokemonDetails = async (pokemonName) => {
-      try {
-        const response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-        );
-        const { name, id, types, abilities, moves } = response.data;
-        setPokemonInfo({
-          name,
-          id,
-          type: types.map((typeInfo) => typeInfo.type.name).join(", "),
-          abilities: abilities.map((abilityInfo) => abilityInfo.ability.name),
-          moves: moves.map((moveInfo) => moveInfo.move.name),
-        });
-        setShowPokemonInfoModal(true);
-      } catch (error) {
-        console.error("Failed to fetch Pokémon details:", error);
-      }
-    };
-
-    fetchPokemonDetails(pokemon.name);
+  const handleImageClick = (pokemon) => {
+    setSelectedPokemon(pokemon);
+    setShowPopup(true);
   };
+  const handlePokemonInfo = (pokemon) => {
+    const pokemonDetail = pokemonInfo.find((p) => p.name === pokemon.name);
+    if (pokemonDetail) {
+      setPokemonInfo({
+        name: pokemonDetail.name,
+        id: pokemonDetail.id,
+        type: pokemonDetail.types
+          .map((typeInfo) => typeInfo.type.name)
+          .join(", "),
+        abilities: pokemonDetail.abilities.map(
+          (abilityInfo) => abilityInfo.ability.name
+        ),
+        moves: pokemonDetail.moves.map((moveInfo) => moveInfo.move.name),
+      });
+      setShowPokemonInfoModal(true);
+    } else {
+      console.error("Pokémon details not found.");
+    }
+  };
+
   return (
     <Layout title={"Account"}>
+      <Head>
+        <title>Pokédex - AccountPage</title>
+        <meta property="og:title" content="Pokédex - Homepage" />
+        <meta property="og:description" content="Find your favorite pokémon" />
+        <meta property="og:image" content="/banner.png" />
+        <meta property="og:url" content="https:pokemon-main-kohl.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="image card" />
+        <meta name="twitter:title" content="Pokédex - Homepage" />
+        <meta name="twitter:description" content="Find your favorite pokémon" />
+        <meta name="twitter:image" content="/banner.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+      </Head>
       <div className="flex">
         <>
           <CreateTeam
@@ -133,18 +168,18 @@ export default function AccountPage() {
             fetchDataUserTeams={fetchDataUserTeams}
             selectedTeamIndex={selectedTeamIndex}
             teamPokemon={teamPokemon}
-            handleTeamClick={handleTeamClick} // Thêm prop này
+            handleTeamClick={handleTeamClick}
             setTeamPokemon={setTeamPokemon}
             setSelectedTeamIndex={setSelectedTeamIndex}
-            setShowPokemonModal={setShowPokemonModal} // Truyền thêm prop này
-            setSelectedPokemon={setSelectedPokemon} // Truyền thêm prop này
-            setShowAbilityMoveModal={setShowAbilityMoveModal} // Truyền thêm prop này
-            setShowPokemonInfoModal={setShowPokemonInfoModal} // Truyền thêm prop này
-            setPokemonInfo={setPokemonInfo} // Truyền thêm prop này
+            setShowPokemonModal={setShowPokemonModal}
+            setSelectedPokemon={setSelectedPokemon}
+            setShowAbilityMoveModal={setShowAbilityMoveModal}
+            setShowPokemonInfoModal={setShowPokemonInfoModal}
+            setPokemonInfo={setPokemonInfo}
           />
         </>
         <div className="flex flex-col">
-          {showPokemonModal && ( // Di chuyển từ CreateTeam
+          {showPokemonModal && (
             <PokemonSelectionModal
               onSave={(pokemon) => {
                 if (selectedPokemon) {
@@ -156,28 +191,26 @@ export default function AccountPage() {
                 } else {
                   addPokemonToTeam(pokemon);
                 }
-                setSelectedPokemon(null); // Reset selected Pokémon
+                setSelectedPokemon(null);
                 setShowPokemonModal(false);
               }}
               onClose={() => setShowPokemonModal(false)}
             />
           )}
-          {showAbilityMoveModal &&
-            selectedPokemon && ( // Di chuyển từ CreateTeam
-              <AbilityMoveSelectionModal
-                pokemon={selectedPokemon}
-                onSave={handleSaveAbilityMove}
-                onClose={() => setShowAbilityMoveModal(false)}
-              />
-            )}
+          {showAbilityMoveModal && selectedPokemon && (
+            <AbilityMoveSelectionModal
+              pokemon={selectedPokemon}
+              onSave={handleSaveAbilityMove}
+              onClose={() => setShowAbilityMoveModal(false)}
+            />
+          )}
 
-          {showPokemonInfoModal &&
-            pokemonInfo && ( // Di chuyển từ CreateTeam
-              <PokemonInformationModal
-                pokemon={pokemonInfo}
-                onClose={() => setShowPokemonInfoModal(false)}
-              />
-            )}
+          {showPokemonInfoModal && pokemonInfo && (
+            <PokemonInformationModal
+              pokemon={pokemonInfo}
+              onClose={() => setShowPokemonInfoModal(false)}
+            />
+          )}
 
           {selectedTeamIndex !== null &&
             userData &&
@@ -187,31 +220,32 @@ export default function AccountPage() {
                   Choose Pokémon
                   <div className="flex gap-3">
                     {teamPokemon.map((pokemon) => {
+                      const pokemonDetail = pokemonInfo.find(
+                        (p) => p.name === pokemon.name
+                      );
+
                       return (
                         <div
                           key={pokemon.name}
                           className="flex items-center gap-3 justify-between p-2 bg-gray-100 rounded mb-2"
                         >
-                          <span>{pokemon.name}</span>
+                          <img
+                            onClick={() => handleImageClick(pokemon)}
+                            src={
+                              pokemonDetail
+                                ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonDetail.id}.png`
+                                : "/placeholder.png"
+                            }
+                            alt={pokemon.name}
+                            className="w-16 h-16 hover:cursor-pointer"
+                          />
 
-                          <button
-                            className="text-blue-500  bg-yellow-400 "
-                            onClick={() => handlePokemonInfo(pokemon)}
-                          >
-                            Info
-                          </button>
-                          <button
-                            className="text-blue-500 bg-primary "
-                            onClick={() => handleEditPokemon(pokemon)}
-                          >
-                            Edit
-                          </button>
-                          <button
+                          {/* <button
                             className="text-fire p-2 bg-gray-700"
                             onClick={() => handleSelectPokemonModal(pokemon)}
                           >
                             Select New Pokémon
-                          </button>
+                          </button> */}
                         </div>
                       );
                     })}
@@ -222,7 +256,27 @@ export default function AccountPage() {
                     )}
                   </div>
                 </div>
-                <div className="h-80 w-785px bg-steel">Information</div>
+                <div className="h-80 w-785px bg-steel p-4">
+                  {selectedTeamIndex !== null &&
+                  userData &&
+                  userData.teams[selectedTeamIndex] ? (
+                    userData.teams[selectedTeamIndex].team.map(
+                      (pokemon, index) => (
+                        <div key={index} className="mb-4">
+                          <h2 className="text-lg font-bold">{pokemon.name}</h2>
+                          <p>
+                            <strong>Ability:</strong> {pokemon.ability}
+                          </p>
+                          <p>
+                            <strong>Moves:</strong> {pokemon.moves.join(", ")}
+                          </p>
+                        </div>
+                      )
+                    )
+                  ) : (
+                    <p>No team selected or team data unavailable.</p>
+                  )}
+                </div>{" "}
                 <button
                   onClick={handleSaveTeam}
                   className="mt-4 p-2 bg-green-500 text-white rounded hover:bg-green-700"
@@ -232,21 +286,15 @@ export default function AccountPage() {
               </>
             )}
         </div>
+        {showPopup && selectedPokemon && (
+          <PokemonOptionPopup
+            pokemon={selectedPokemon}
+            onEdit={() => handleEditPokemon(selectedPokemon)}
+            onSelectNew={() => handleSelectNewPokemon(selectedPokemon)}
+            onClose={() => setShowPopup(false)}
+          />
+        )}
       </div>
     </Layout>
   );
 }
-
-// <Head>
-// <title>Pokédex - AccountPage</title>
-// <meta property="og:title" content="Pokédex - Homepage" />
-// <meta property="og:description" content="Find your favorite pokémon" />
-// <meta property="og:image" content="/banner.png" />
-// <meta property="og:url" content="https://pokemon-main-kohl.vercel.app/" />
-// <meta property="og:type" content="website" />
-// <meta name="twitter:card" content="image card" />
-// <meta name="twitter:title" content="Pokédex - Homepage" />
-// <meta name="twitter:description" content="Find your favorite pokémon" />
-// <meta name="twitter:image" content="/banner.png" />
-// <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
-// </Head>
